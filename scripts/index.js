4f3e21a131af86529992b0bf80060c1b51d4fa57
Kate Kulumbegova <ekulumbegova@yandex.ru>
2022-03-18 00:18:40 +1000

    Отправка на 2 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,27 @@
 let editButton = document.querySelector('.profile__editButton');
 let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.close-btn');
+let closeButton = document.querySelector('.popup__close-btn');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
-let inputName = document.querySelector('.popup__input_name');
-let inputJob = document.querySelector('.popup__input_job');
+let inputName = document.querySelector('.form__input_type_name');
+let inputJob = document.querySelector('.form__input_type_job');
 let formElement = document.querySelector('.popup__container');
-let submitButton = document.querySelector('.popup__submit');
+let submitButton = document.querySelector('.form__submit');
 
-editButton.addEventListener('click', function() {
+
+inputName.value = profileName.textContent;
+inputJob.value = profileJob.textContent;
+
+function openPopUp() {
     popup.classList.add('popup_opened');
-    inputName.value = profileName.textContent;
-    inputJob.value = profileJob.textContent;
-})
+}
 
-closeButton.addEventListener('click', function() {
-        popup.classList.remove('popup_opened');
-})
+function closePopUp() {
+    popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopUp) 
+closeButton.addEventListener('click', closePopUp)
 
 
 
@@ -24,9 +29,7 @@ function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
-
-        popup.classList.remove('popup_opened');
-
+    closePopUp();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
