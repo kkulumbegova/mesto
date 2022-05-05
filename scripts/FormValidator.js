38415298ed68043eb03aa2ee7218095f5520385d
Kate Kulumbegova <ekulumbegova@yandex.ru>
2022-05-05 22:52:26 +1000

    Проект№7(3 коммит)

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -4,68 +4,88 @@ class FormValidator {
     this._validationConfig = validationConfig;
   }
   _showInputError = (inputElement, errorMessage) => {
-    const errorElement = inputElement.parentNode.querySelector(`#${inputElement.id}-error`);// Находим элемент ошибки внутри функции
+    const errorElement = inputElement.parentNode.querySelector(
+      `#${inputElement.id}-error`
+    ); // Находим элемент ошибки внутри функции
     inputElement.classList.add(`${this._validationConfig.inputErrorClass}`);
     errorElement.textContent = errorMessage;
-  }
-    // функция удаления отображения ошибки
+  };
+  // функция удаления отображения ошибки
   _hideInputError = (inputElement) => {
-    const errorElement = inputElement.parentNode.querySelector(`#${inputElement.id}-error`);
-    errorElement.textContent = '';
+    const errorElement = inputElement.parentNode.querySelector(
+      `#${inputElement.id}-error`
+    );
+    errorElement.textContent = "";
     inputElement.classList.remove(`${this._validationConfig.inputErrorClass}`);
-  }
-    //функция обнуления всех инпутов, ошибка скрывается, кнопка сохранить неактивна
+  };
+  //функция обнуления всех инпутов, ошибка скрывается, кнопка сохранить неактивна
   getEmpty = () => {
-    const inputList = Array.from(this._formElement.querySelectorAll(`${this._validationConfig.inputSelector}`));
-     inputList.forEach(input => {
-       this._hideInputError(input);
-     });
-     this._formElement.reset();
-     const buttonSubmit = this._formElement.querySelector(`${this._validationConfig. submitButtonSelector}`);
-     buttonSubmit.classList.add(`${this._validationConfig.inactiveButtonClass}`);
-  }
+    const inputList = Array.from(
+      this._formElement.querySelectorAll(
+        `${this._validationConfig.inputSelector}`
+      )
+    );
+    inputList.forEach((input) => {
+      this._hideInputError(input);
+    });
+    this._formElement.reset();
+    const buttonSubmit = this._formElement.querySelector(
+      `${this._validationConfig.submitButtonSelector}`
+    );
+    buttonSubmit.classList.add(`${this._validationConfig.inactiveButtonClass}`);
+  };
 
   _isValid = (inputElement) => {
-    if(!inputElement.validity.valid) {
+    if (!inputElement.validity.valid) {
       this._showInputError(inputElement, inputElement.validationMessage);
     } else {
       this._hideInputError(inputElement);
     }
-  }
-    
-    //функция проверки валидны ли ВСЕ инпуты
+  };
+
+  //функция проверки валидны ли ВСЕ инпуты
   _hasInvalidInput = (inputList) => {
-    return inputList.some((inputElement) => {  
+    return inputList.some((inputElement) => {
       return !inputElement.validity.valid;
     });
-  }; 
+  };
 
-//функция блокировки кнопки, если хотя бы один инпут не валиден
+  //функция блокировки кнопки, если хотя бы один инпут не валиден
   _toggleButton = (inputList, buttonElement) => {
-    if(this._hasInvalidInput(inputList)) {
-      buttonElement.classList.add(`${this._validationConfig.inactiveButtonClass}`);
+    if (this._hasInvalidInput(inputList)) {
+      buttonElement.classList.add(
+        `${this._validationConfig.inactiveButtonClass}`
+      );
     } else {
-      buttonElement.classList.remove(`${this._validationConfig.inactiveButtonClass}`);
+      buttonElement.classList.remove(
+        `${this._validationConfig.inactiveButtonClass}`
+      );
     }
-  }
-//функция проверки на валидность всем полям формы
+  };
+  //функция проверки на валидность всем полям формы
   _setEventListeners = () => {
-    const inputList = Array.from(this._formElement.querySelectorAll(`${this._validationConfig.inputSelector}`)); //находим все поля в документе. делаем массив из коллекции
-    const buttonElement = this._formElement.querySelector(`${this._validationConfig.submitButtonSelector}`);
+    const inputList = Array.from(
+      this._formElement.querySelectorAll(
+        `${this._validationConfig.inputSelector}`
+      )
+    ); //находим все поля в документе. делаем массив из коллекции
+    const buttonElement = this._formElement.querySelector(
+      `${this._validationConfig.submitButtonSelector}`
+    );
     this._toggleButton(inputList, buttonElement);
-    inputList.forEach(inputElement => {
-      inputElement.addEventListener('input', () => {
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
         this._isValid(inputElement);
         this._toggleButton(inputList, buttonElement);
       });
     });
   };
   enableValidation = () => {
-    this._formElement.addEventListener('submit', (evt) => {
+    this._formElement.addEventListener("submit", (evt) => {
       evt.preventDefault();
     });
     this._setEventListeners(this._formElement);
-  }
+  };
 }
 
 export default FormValidator;
