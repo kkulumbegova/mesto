5a9b08b6d36b11d2cd20194fd3d8dea719d36e9c
Kate Kulumbegova <ekulumbegova@yandex.ru>
2022-06-07 07:11:24 +1000

    Проект №9(3)

--- src/components/PopupWithImg.js
+++ src/components/PopupWithImg.js
@@ -3,8 +3,8 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(selector) {
     super(selector);
-    this._imagePopup = document.querySelector(".popup__img");
-    this._imageName = document.querySelector(".popup__card-name");
+    this._imagePopup = this.__popupElement.querySelector(".popup__img");
+    this._imageName = this.__popupElement.querySelector(".popup__card-name");
   }
   open(name, link) {
     this._imagePopup.src = link;
